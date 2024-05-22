9298bdf1ab4d953a218800caaf92dbb1f5097d85
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2024-05-22 10:44:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'https://mirano-nu.vercel.app/', // Замените на URL вашего фронтенда
+  origin: 'https://mirano-nu.vercel.app', // Замените на URL вашего фронтенда
   credentials: true,
 };
 
